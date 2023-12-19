0bdc28255e09d67d6c54fe31f1de3764aaa90b76
Jonasbahizi2023 <125441384+Jonasbahizi2023@users.noreply.github.com>
2023-12-19 11:53:53 +0200

    initial commit

---
+++ src/main/java/com/example/hospitalambulance/HospitalAmbulanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospitalambulance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAmbulanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAmbulanceApplication.class, args);
+	}
+
+}
